60d90ddde7ad0800074a8264f87e856ab129f070
Charla CCSky <ceansky@gmail.com>
2017-11-13 19:58:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,3 +2,27 @@
 def position_taken?(board, index)
 
 end
+
+
+
+
+
+
+
+
+
+=begin 
+def valid_move?(board,index)
+  if index.between?(0,8) && position_taken?(board,index)==false
+    return true
+  else
+  end
+end
+
+def position_taken?(board,index)
+  if board[index]==" "||board[index]==""||board[index]==nil
+    return false
+  else
+  end
+end
+=end
